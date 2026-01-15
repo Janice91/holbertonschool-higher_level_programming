9c79a6e330f245f20c8ded450f5f44cd6343c284
Janice91 <Kouakou.janice.2002@gmail.com>
2026-01-15 14:43:15 +0000

    Fix 8-uppercase.py: print string in uppercase using str.format and one loop

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("".join(chr(ord(c) - 32) if "a" <= c <= "z" else c for c in str))
+    """Print a string in uppercase using only one loop and str.format()"""
+    print("{}".format("".join(chr(ord(c) - 32) if "a" <= c <= "z" else c for c in str)))
