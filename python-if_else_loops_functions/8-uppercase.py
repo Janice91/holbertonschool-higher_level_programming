374f6dda9650822715b2f4ccfd1d701c092b6473
Janice91 <Kouakou.janice.2002@gmail.com>
2026-01-15 14:45:02 +0000

    Fix 8-uppercase.py: print string in uppercase using one loop and str.format(), pycodestyle OK

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Print a string in uppercase using only one loop and str.format()"""
-    print("{}".format("".join(chr(ord(c) - 32) if "a" <= c <= "z" else c for c in str)))
+    result = "".join(
+        chr(ord(c) - 32) if "a" <= c <= "z" else c
+        for c in str
+    )
+    print("{}".format(result))
