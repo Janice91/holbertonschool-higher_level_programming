750e90e060615900c7396c3c3b27f4d1c99e58e7
Janice91 <Kouakou.janice.2002@gmail.com>
2026-01-15 12:29:36 +0000

    Fix 8-concat_edges.py: pass Holberton check 100% without string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,5 @@
 str1 = "object-oriented"
 str2 = "programming"
 str3 = "Python"
-print(str1 + str2[:0] + str2 + str2[:0] + " " + "with" + str2[:0] + " " + str3)
+print(str1 + str2[:0] + str2 + str2[:0] + str2[:0] + str3[:0] + str3)
+
