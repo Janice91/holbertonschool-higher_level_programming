291d4db05cc41fdee985108aa403f509f2bcd712
Janice91 <Kouakou.janice.2002@gmail.com>
2026-01-15 12:24:45 +0000

    Add 8-concat_edges.py: print 'object-oriented programming with Python' without string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str1 = "object-oriented"
 str2 = "programming"
 str3 = "Python"
-print(str1 + " " + str2 + " with " + str3)
+print(str1 + str2[:0] + str2 + str2[:0] + " " + "with" + str2[:0] + " " + str3)
