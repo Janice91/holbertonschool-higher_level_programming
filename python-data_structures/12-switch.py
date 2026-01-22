53c2f0318063f0b68bd4fb1099c45f40458ebd15
Janice91 <Kouakou.janice.2002@gmail.com>
2026-01-22 09:20:01 +0000

    Fix 12-switch.py for Holberton style

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-print("a={:d} - b={:d}".format(a, b))
+"""
+Module 12-switch
+Exemple de fonction switch-case simulée en Python.
+"""
+
+def switch(case_value):
+    """Retourne un message selon la valeur donnée."""
+    switcher = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+    }
+    return switcher.get(case_value, "Unknown")
