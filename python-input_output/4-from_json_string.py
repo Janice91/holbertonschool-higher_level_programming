75b4e57100a98d4ca7a3394f558f1d3f38409031
Janice91 <Kouakou.janice.2002@gmail.com>
2026-02-12 08:36:48 +0000

    Add task 4: from_json_string function

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to convert JSON string to object"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) from a JSON string
+    """
+    return json.loads(my_str)
