ca241c061360eea85508e65cea4b8732fc2d0f15
Janice91 <Kouakou.janice.2002@gmail.com>
2026-02-12 09:16:24 +0000

    Fix task 10: simplify to_json method

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,33 +6,13 @@ class Student:
     """Class that defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance
-
-        Args:
-            first_name: First name of the student
-            last_name: Last name of the student
-            age: Age of the student
-        """
+        """Initializes a Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance
-
-        Args:
-            attrs: List of attribute names to retrieve (optional)
-
-        Returns:
-            Dictionary with all attributes or filtered attributes
-        """
-        if attrs is None:
-            return self.__dict__
-        
-        filtered_dict = {}
-        for attr in attrs:
-            if hasattr(self, attr):
-                filtered_dict[attr] = getattr(self, attr)
-        return filtered_dict
+        """Retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
