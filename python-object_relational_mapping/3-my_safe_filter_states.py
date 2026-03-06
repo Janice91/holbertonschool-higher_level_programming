a245fac181fc5d89866182554b6bcdab2b8d7ad2
Janice91 <Kouakou.janice.2002@gmail.com>
2026-03-06 13:57:25 +0000

    Fix task 3: BINARY with parameterized query

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,11 +12,10 @@ if __name__ == "__main__":
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
                 (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     conn.close()
-
